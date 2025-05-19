486a25a3b34b14d78b1ca0f83cda2effc002cfe3
regenerationdream <regenerationdream@gmail.com>
2025-05-19 20:08:10 +0300

    Элементы навигации вынесены в отдельный файл

--- edicewebapp/src/components/Navigation/Navigation.jsx
+++ edicewebapp/src/components/Navigation/Navigation.jsx
@@ -1,17 +1,11 @@
 import './Navigation.css'
 import NavigationList from "./NavigationList.jsx";
+import {MenuItems} from "./NavigationItemsData.js";
 
 function Navigation() {
-    let menuItems = [
-        {text: 'Главная', href: '/', image: 'src/assets/templateIcon.svg'},
-        {text: 'Лобби', href: '/lobby', image: 'src/assets/templateIcon.svg'},
-        {text: 'Библиотека', href: '/library', image: 'src/assets/templateIcon.svg'},
-        {text: 'Конструктор', href: '/constructor', image: 'src/assets/templateIcon.svg'},
-    ]
-
     return (
         <div className='navigation-container'>
-            <NavigationList items={menuItems}/>
+            <NavigationList items={MenuItems}/>
         </div>
     )
 }
