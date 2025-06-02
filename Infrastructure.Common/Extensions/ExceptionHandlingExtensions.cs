61d07abc867e0279e099f85deb3aa281b748fe01
e-vasilev <e-vasilyev@asoft21.ru>
2025-06-02 21:15:11 +0300

    feat(exceptionLogging): middleware для логирования исключений

---
+++ Infrastructure.Common/Extensions/ExceptionHandlingExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Common.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace Infrastructure.Common.Extensions;
+
+public static class ExceptionHandlingExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+    {
+        builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        return builder;
+    }
+}
---
+++ Infrastructure.Common/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Shared.Logging;
+
+namespace Infrastructure.Common.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IAppLogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, IAppLogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unhandled error occurred");
+            context.Response.StatusCode = 500;
+        }
+    }
+}
