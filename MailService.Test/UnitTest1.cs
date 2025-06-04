c8d9f12f892ba256133cd6446d157a84d176b88c
dmitrygalkin <dgalkin@chuvsu.ru>
2025-06-04 19:44:47 +0300

    test: создал проект для тестов MailService.csproj

---
+++ MailService.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MailService.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void SendMail()
+    {
+        Assert.Pass();
+    }
+}
