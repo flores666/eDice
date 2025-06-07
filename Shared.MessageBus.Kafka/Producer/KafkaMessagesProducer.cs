59acebebc77cb77aac7711a6f54f05638b768906
e-vasilev <e-vasilyev@asoft21.ru>
2025-06-07 10:49:18 +0300

    chore(KafkaMessagesProducer): логирование

--- Shared.MessageBus.Kafka/Producer/KafkaMessagesProducer.cs
+++ Shared.MessageBus.Kafka/Producer/KafkaMessagesProducer.cs
@@ -1,14 +1,17 @@
 ﻿using Confluent.Kafka;
+using Shared.Logging;
 using Shared.MessageBus.Kafka.Serializer;
 
 namespace Shared.MessageBus.Kafka.Producer;
 
 public class KafkaMessagesProducer<TMessage> : IMessagesProducer<TMessage>
 {
+    private readonly IAppLogger<KafkaMessagesProducer<TMessage>> _logger;
     private readonly IProducer<Guid, TMessage> _producer;
 
-    public KafkaMessagesProducer()
+    public KafkaMessagesProducer(IAppLogger<KafkaMessagesProducer<TMessage>> logger)
     {
+        _logger = logger;
         var config = new ProducerConfig
         {
             BootstrapServers = KafkaOptions.BootstrapServers
@@ -18,11 +21,16 @@ public class KafkaMessagesProducer<TMessage> : IMessagesProducer<TMessage>
             .SetValueSerializer(new KafkaValueSerializer<TMessage>())
             .SetKeySerializer(new KafkaKeySerializer())
             .Build();
+        
+        _logger.LogInformation("Ready!");
     }
     
     public async Task<Guid> PublishAsync(string topic, TMessage message, CancellationToken cancellationToken = default)
     {
         var key = Guid.NewGuid();
+        
+        if (_logger.IsDebugEnabled) _logger.LogDebug("Published message with key {K}", key);
+        
         await _producer.ProduceAsync(topic, new Message<Guid, TMessage>
         {
             Key = key,
@@ -35,6 +43,7 @@ public class KafkaMessagesProducer<TMessage> : IMessagesProducer<TMessage>
 
     public void Dispose()
     {
+        if (_logger.IsDebugEnabled) _logger.LogDebug("Disposed");
         _producer?.Dispose();
     }
 }
